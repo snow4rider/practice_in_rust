e34f5713446827b443defc15b913521235fac0e7
Matthew <matthew.t.mckenney@gmail.com>
2022-04-13 18:57:46 -0600

    chapter 9

---
+++ chapter_9/open_file/target/rls/debug/deps/open_file-8b6e51146dd9733f.d
@@ -0,0 +1,5 @@
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_9\open_file\target\rls\debug\deps\open_file-8b6e51146dd9733f.rmeta: src\main.rs
+
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_9\open_file\target\rls\debug\deps\open_file-8b6e51146dd9733f.d: src\main.rs
+
+src\main.rs:
---
+++ chapter_9/open_file/target/rls/debug/deps/open_file-f73e402685df0e98.d
@@ -0,0 +1,5 @@
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_9\open_file\target\rls\debug\deps\open_file-f73e402685df0e98.rmeta: src\main.rs
+
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_9\open_file\target\rls\debug\deps\open_file-f73e402685df0e98.d: src\main.rs
+
+src\main.rs:
