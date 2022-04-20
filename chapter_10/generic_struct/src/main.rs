5025e52f50f6abb581bf7197e95242e4d6a82cb3
Matthew <matthew.t.mckenney@gmail.com>
2022-04-19 18:00:34 -0600

    chapter 10

--- chapter_10/generic_struct/src/main.rs
+++ chapter_10/generic_struct/src/main.rs
@@ -6,4 +6,7 @@ struct Point<T> {
 fn main() {
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
+
+    println!("{} {}", integer.x, integer.y);
+    println!("{} {}", float.x, float.y);
 }
