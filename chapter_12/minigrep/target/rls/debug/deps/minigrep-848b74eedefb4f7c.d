ccbc9a3f6a618916505975b040dfa92519a1c43c
Matthew <matthew.t.mckenney@gmail.com>
2022-04-30 07:59:01 -0600

    chapter 12
    starting on CLI mini grep

---
+++ chapter_12/minigrep/target/rls/debug/deps/minigrep-848b74eedefb4f7c.d
@@ -0,0 +1,5 @@
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\rls\debug\deps\minigrep-848b74eedefb4f7c.rmeta: src\main.rs
+
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\rls\debug\deps\minigrep-848b74eedefb4f7c.d: src\main.rs
+
+src\main.rs:
---
+++ chapter_12/minigrep/target/rls/debug/deps/minigrep-a5f7bd58b3dc7378.d
@@ -0,0 +1,5 @@
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\rls\debug\deps\minigrep-a5f7bd58b3dc7378.rmeta: src\main.rs
+
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\rls\debug\deps\minigrep-a5f7bd58b3dc7378.d: src\main.rs
+
+src\main.rs:
