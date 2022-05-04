5de8a45b2f829ca15087e323fc5500610d7c0ab1
snow4rider <matthew.t.mckenney@gmail.com>
2022-05-04 08:27:03 -0600

    Chapter 12

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -13,9 +13,13 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
-    
+
     println!("With text:\n{}", contents);
 }
 
