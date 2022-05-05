48424c057314b8ad6b3e5e34a03a5195a0078ee8
snow4rider <matthew.t.mckenney@gmail.com>
2022-05-05 14:36:24 -0600

    linkedin learning

--- linkedin_learning/bitwise_prac/src/main.rs
+++ linkedin_learning/bitwise_prac/src/main.rs
@@ -9,4 +9,13 @@ fn main() {
     value = value & 0b1111_0111;
     println!("value is {:08b}", value);
     println!("bit 6 is {}", value & 0b0100_0000);
+
+    value = value ^ 0b0101_0101;
+    println!("value is {:08b}", value);
+
+    value = value << 4;
+    println!("value is {:08b}", value);
+
+    value = value >> 2;
+    println!("value is {:08b}", value);
 }
