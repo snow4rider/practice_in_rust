2f79365e1a6972e29c34609a1706194be18a7275
Matthew <matthew.t.mckenney@gmail.com>
2022-05-15 17:29:15 -0600

    learning

--- linkedin_learning/chapter_07/slice_prac/src/main.rs
+++ linkedin_learning/chapter_07/slice_prac/src/main.rs
@@ -2,5 +2,6 @@ fn main() {
     let message = String::from("Greetings from earth!");
     println!("message is {message}");
 
-    let last_word = &message;
+    let last_word = &message[15..15+5];
+    println!("last_word is {last_word}");
 }
