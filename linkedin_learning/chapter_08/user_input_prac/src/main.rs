4252b9565fe60442885fd2aab9defb54e802d98d
snow4rider <matthew.t.mckenney@gmail.com>
2022-05-19 07:37:54 -0600

    learning

--- linkedin_learning/chapter_08/user_input_prac/src/main.rs
+++ linkedin_learning/chapter_08/user_input_prac/src/main.rs
@@ -5,5 +5,8 @@ fn main() {
     println!("Enter a message:");
     io::stdin().read_line(&mut buffer);
     println!("buffer is {}", buffer);
+
+    let number: i32 = buffer.trim().parse().unwrap();
+    println!("number + 1 is {}", number + 1);
     
 }
