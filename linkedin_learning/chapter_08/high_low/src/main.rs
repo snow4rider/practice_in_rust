c7b152781f732d88f3d81e1cb5fdc548f7c75c0a
snow4rider <matthew.t.mckenney@gmail.com>
2022-05-19 14:03:36 -0600

    learning

--- linkedin_learning/chapter_08/high_low/src/main.rs
+++ linkedin_learning/chapter_08/high_low/src/main.rs
@@ -2,20 +2,25 @@ use rand::prelude::*;
 use std::io;
 
 fn main() {
-    let mut guess_number = String::new();
+    
     let number: i32 = thread_rng().gen_range(1..=10);
-    println!("{}",number);
+    //println!("{}",number);
 
-    println!("Please guess the number: ");
-    io::stdin().read_line(&mut guess_number);
+    loop {
+        let mut guess_number = String::new();
+        println!("Please guess the number: ");
+        io::stdin().read_line(&mut guess_number)
+                   .expect("Failed to parse the guess.");
+        
+        let guess_number: i32 = guess_number.trim().parse().unwrap();
 
-    let guess_number: i32 = guess_number.trim().parse().unwrap();
-    if guess_number == number {
-        println!("you guessed right!");
-    } else if guess_number > number {
-        println!("You guessed too high.");
-    } else {
-        println!("You guessed too low.");
+        if guess_number == number {
+            println!("you guessed right!");
+            break;
+        } else if guess_number > number {
+            println!("You guessed too high.");
+        } else {
+            println!("You guessed too low.");
+        }
     }
-
 }
