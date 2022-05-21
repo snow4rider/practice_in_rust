9a87f232c989b68fcc82c386244c159498ff6153
Matthew <matthew.t.mckenney@gmail.com>
2022-05-21 12:52:13 -0600

    learning

--- linkedin_learning/chapter_09/command_line_prac/src/main.rs
+++ linkedin_learning/chapter_09/command_line_prac/src/main.rs
@@ -4,5 +4,8 @@ fn main() {
     for (index, argument) in env::args().enumerate() {
         println!("argument {} is {}", index, argument);
     }
+
+    let arg2 = env::args().nth(2).unwrap();
+    println!("arg2 is {}", arg2);
     
 }
