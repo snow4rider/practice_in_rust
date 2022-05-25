5cfbc08bbec731469fe81f3cadd9f56e6a125d8f
Matthew <matthew.t.mckenney@gmail.com>
2022-05-24 18:02:42 -0600

    learning

--- linkedin_learning/chapter_10/struct_prac/src/main.rs
+++ linkedin_learning/chapter_10/struct_prac/src/main.rs
@@ -4,12 +4,29 @@ struct Shuttle {
     propellant: f64
 }
 
+impl Shuttle {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    fn add_fuel(&mut self, gallons: f64) {
+        self.propellant += gallons;
+    }
+}
+
 fn main() {
-    let vehicle = Shuttle{
+    let mut vehicle = Shuttle{
         name: String::from("Endeavour"),
         crew_size:7,
-        propellant: 835958.0
+        propellant: 0.0
     };
+
+    let vehicle_name = vehicle.get_name();
+    println!("vehicle name is {}", vehicle_name);
+
+    println!("propellant is {}", vehicle.propellant);
+    vehicle.add_fuel(1000.0);
+    println!("propellant is {}", vehicle.propellant);
 
     let vehicle02 = Shuttle {
         name: String::from("Discovery"),
