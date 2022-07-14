e808a80270922873ffed9c83de11c7c2b869e899
Matthew <matthew.t.mckenney@gmail.com>
2022-07-13 23:06:45 -0600

    the book

--- chapter_12/minigrep/src/lib.rs
+++ chapter_12/minigrep/src/lib.rs
@@ -25,6 +25,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
---
+++ chapter_12/minigrep/target/debug/deps/minigrep-abd9d49fcd24a891.d
@@ -0,0 +1,5 @@
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\debug\deps\minigrep-abd9d49fcd24a891.exe: src\lib.rs
+
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\debug\deps\minigrep-abd9d49fcd24a891.d: src\lib.rs
+
+src\lib.rs:
