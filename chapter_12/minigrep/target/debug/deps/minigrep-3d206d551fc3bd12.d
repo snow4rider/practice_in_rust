f5c825a5390a497d9f37ad19a18f79869632cb77
Matthew <matthew.t.mckenney@gmail.com>
2022-07-13 23:19:13 -0600

    the book

---
+++ chapter_12/minigrep/target/debug/deps/minigrep-3d206d551fc3bd12.d
@@ -0,0 +1,5 @@
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\debug\deps\minigrep-3d206d551fc3bd12.exe: src\main.rs
+
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_12\minigrep\target\debug\deps\minigrep-3d206d551fc3bd12.d: src\main.rs
+
+src\main.rs:
