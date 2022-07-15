90720150d208e46d98845b69c6642da518e0a1ea
Matthew <matthew.t.mckenney@gmail.com>
2022-07-15 15:49:34 -0600

    the book

---
+++ chapter_13/inventory/target/debug/inventory.d
@@ -0,0 +1,1 @@
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_13\inventory\target\debug\inventory.exe: C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_13\inventory\src\main.rs
---
+++ chapter_13/inventory/target/rls/debug/deps/inventory-cadc590a0851cf35.d
@@ -0,0 +1,5 @@
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_13\inventory\target\rls\debug\deps\inventory-cadc590a0851cf35.rmeta: src\main.rs
+
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_13\inventory\target\rls\debug\deps\inventory-cadc590a0851cf35.d: src\main.rs
+
+src\main.rs:
