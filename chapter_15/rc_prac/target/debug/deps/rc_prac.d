9b5664869d4f4db29b5962e8b70cfeca328cd87e
Matthew <matthew.t.mckenney@gmail.com>
2022-09-28 17:36:53 -0600

    chapter 15

---
+++ chapter_15/rc_prac/target/debug/deps/rc_prac.d
@@ -0,0 +1,5 @@
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_15\rc_prac\target\debug\deps\rc_prac.exe: src\main.rs
+
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_15\rc_prac\target\debug\deps\rc_prac.d: src\main.rs
+
+src\main.rs:
---
+++ chapter_15/rc_prac/target/debug/rc_prac.d
@@ -0,0 +1,1 @@
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_15\rc_prac\target\debug\rc_prac.exe: C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_15\rc_prac\src\main.rs
