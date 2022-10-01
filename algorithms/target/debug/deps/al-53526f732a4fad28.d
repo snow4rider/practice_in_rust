22f9ba25d4f5241dc40b9ab58f8f2ce5eca39dd2
Matthew <matthew.t.mckenney@gmail.com>
2022-10-01 09:22:29 -0600

    chapter 15

---
+++ algorithms/target/debug/deps/al-53526f732a4fad28.d
@@ -0,0 +1,5 @@
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\algorithms\target\debug\deps\al-53526f732a4fad28.rmeta: al\src\main.rs
+
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\algorithms\target\debug\deps\al-53526f732a4fad28.d: al\src\main.rs
+
+al\src\main.rs:
---
+++ algorithms/target/debug/deps/al-b977e05523b946c1.d
@@ -0,0 +1,5 @@
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\algorithms\target\debug\deps\al-b977e05523b946c1.rmeta: al\src\main.rs
+
+c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\algorithms\target\debug\deps\al-b977e05523b946c1.d: al\src\main.rs
+
+al\src\main.rs:
