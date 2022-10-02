2dfe7957ec7d3c99b7452c3aa799ada9932dec35
Matthew <matthew.t.mckenney@gmail.com>
2022-10-02 15:59:57 -0600

    chapter 16

---
+++ chapter_16/channel_prac/target/debug/channel_prac.d
@@ -0,0 +1,1 @@
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_16\channel_prac\target\debug\channel_prac.exe: C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_16\channel_prac\src\main.rs
