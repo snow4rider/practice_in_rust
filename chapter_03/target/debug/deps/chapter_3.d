3a6b09ad2ab1f335d3c486504071ac9b13276929
Matthew <matthew.t.mckenney@gmail.com>
2022-10-01 23:05:50 -0600

    update

--- chapter_03/target/debug/deps/chapter_3.d
+++ chapter_03/target/debug/deps/chapter_3.d
@@ -1,5 +1,5 @@
-C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_3\target\debug\deps\chapter_3.exe: src\main.rs
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_03\target\debug\deps\chapter_3.exe: src\main.rs
 
-C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_3\target\debug\deps\chapter_3.d: src\main.rs
+C:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_03\target\debug\deps\chapter_3.d: src\main.rs
 
 src\main.rs:
--- chapter_03/target/rls/debug/deps/chapter_3-778d9c6332ef3c1d.d
+++
@@ -1,5 +0,0 @@
-c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_3\target\rls\debug\deps\chapter_3-778d9c6332ef3c1d.rmeta: src\main.rs
-
-c:\Users\matth\OneDrive\Desktop\rust\practice_in_rust\chapter_3\target\rls\debug\deps\chapter_3-778d9c6332ef3c1d.d: src\main.rs
-
-src\main.rs:
