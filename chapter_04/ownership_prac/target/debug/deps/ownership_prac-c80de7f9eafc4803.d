fffc59f789ff469f01f382ee7bc0b0fffc4a178c
snow4rider <matthew.t.mckenney@gmail.com>
2023-11-10 22:05:27 -0700

    commpleted chapter 4.1

---
+++ chapter_04/ownership_prac/target/debug/deps/ownership_prac-c80de7f9eafc4803.d
@@ -0,0 +1,5 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_04/ownership_prac/target/debug/deps/ownership_prac-c80de7f9eafc4803: src/main.rs
+
+/home/fox/Documents/rust/practice_in_rust/chapter_04/ownership_prac/target/debug/deps/ownership_prac-c80de7f9eafc4803.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_04/ownership_prac/target/debug/ownership_prac.d
@@ -0,0 +1,1 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_04/ownership_prac/target/debug/ownership_prac: /home/fox/Documents/rust/practice_in_rust/chapter_04/ownership_prac/src/main.rs
