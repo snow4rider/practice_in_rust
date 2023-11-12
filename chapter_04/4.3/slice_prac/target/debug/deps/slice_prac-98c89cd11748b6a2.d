17b69f5fe22457776056bf33222c2e101cf4c781
snow4rider <matthew.t.mckenney@gmail.com>
2023-11-12 00:04:43 -0700

    finished chapter 4.3

---
+++ chapter_04/4.3/slice_prac/target/debug/deps/slice_prac-98c89cd11748b6a2.d
@@ -0,0 +1,5 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_04/4.3/slice_prac/target/debug/deps/slice_prac-98c89cd11748b6a2: src/main.rs
+
+/home/fox/Documents/rust/practice_in_rust/chapter_04/4.3/slice_prac/target/debug/deps/slice_prac-98c89cd11748b6a2.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_04/4.3/slice_prac/target/debug/slice_prac.d
@@ -0,0 +1,1 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_04/4.3/slice_prac/target/debug/slice_prac: /home/fox/Documents/rust/practice_in_rust/chapter_04/4.3/slice_prac/src/main.rs
