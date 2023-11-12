1f532df2b865ae59b7b459afab594b4baeb34658
snow4rider <matthew.t.mckenney@gmail.com>
2023-11-11 20:08:54 -0700

    finished chapter 4.2

---
+++ chapter_04/4.2/ref_prac/target/debug/deps/ref_prac-e172d2feb5432464.d
@@ -0,0 +1,5 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_04/4.2/ref_prac/target/debug/deps/ref_prac-e172d2feb5432464: src/main.rs
+
+/home/fox/Documents/rust/practice_in_rust/chapter_04/4.2/ref_prac/target/debug/deps/ref_prac-e172d2feb5432464.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_04/4.2/ref_prac/target/debug/ref_prac.d
@@ -0,0 +1,1 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_04/4.2/ref_prac/target/debug/ref_prac: /home/fox/Documents/rust/practice_in_rust/chapter_04/4.2/ref_prac/src/main.rs
