b3d631b4848f2dd92f5332985ff36efd671d4730
snow4rider <matthew.t.mckenney@gmail.com>
2023-12-10 12:02:12 -0700

    finished 5.1 of the book

---
+++ chapter_05/5.1/struct_prac/target/debug/deps/struct_prac-604643b5702d90a6.d
@@ -0,0 +1,5 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.1/struct_prac/target/debug/deps/struct_prac-604643b5702d90a6: src/main.rs
+
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.1/struct_prac/target/debug/deps/struct_prac-604643b5702d90a6.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_05/5.1/struct_prac/target/debug/struct_prac.d
@@ -0,0 +1,1 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.1/struct_prac/target/debug/struct_prac: /home/fox/Documents/rust/practice_in_rust/chapter_05/5.1/struct_prac/src/main.rs
