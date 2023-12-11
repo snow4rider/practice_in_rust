9405c9b2437cbcebecaf479bdea218b619e9f33d
snow4rider <matthew.t.mckenney@gmail.com>
2023-12-11 12:08:51 -0700

    finished chapter 5.3 in the book

---
+++ chapter_05/5.3/rectangle/target/debug/deps/rectangle-671eeb79732b0e9d.d
@@ -0,0 +1,5 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.3/rectangle/target/debug/deps/rectangle-671eeb79732b0e9d: src/main.rs
+
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.3/rectangle/target/debug/deps/rectangle-671eeb79732b0e9d.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_05/5.3/rectangle/target/debug/rectangle.d
@@ -0,0 +1,1 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.3/rectangle/target/debug/rectangle: /home/fox/Documents/rust/practice_in_rust/chapter_05/5.3/rectangle/src/main.rs
