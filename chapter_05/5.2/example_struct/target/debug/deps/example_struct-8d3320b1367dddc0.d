dfd35682c364aff2c263065ac599738074e101de
snow4rider <matthew.t.mckenney@gmail.com>
2023-12-11 10:52:15 -0700

    finnished chapter 5.2

---
+++ chapter_05/5.2/example_struct/target/debug/deps/example_struct-8d3320b1367dddc0.d
@@ -0,0 +1,5 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.2/example_struct/target/debug/deps/example_struct-8d3320b1367dddc0: src/main.rs
+
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.2/example_struct/target/debug/deps/example_struct-8d3320b1367dddc0.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_05/5.2/example_struct/target/debug/example_struct.d
@@ -0,0 +1,1 @@
+/home/fox/Documents/rust/practice_in_rust/chapter_05/5.2/example_struct/target/debug/example_struct: /home/fox/Documents/rust/practice_in_rust/chapter_05/5.2/example_struct/src/main.rs
